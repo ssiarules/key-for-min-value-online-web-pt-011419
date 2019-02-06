47e2b7142df9a86a5e47f80b6d34c2fdf6db2f53
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-06 00:44:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  {aysan => 5, johnson => 2, cernan => 1}
      smallest_value = nil #this will contain our lowest value
      smallest_key = nil  #this will contain the key for our lowest value
      name_hash.each do |name, value| 
        if value < smallest_value #if the value in our hash is lower than our smallest_value, reassign smallest value to value
          smallest_value = value #reassigning smallest_value to lowest value 
          smallest_key = name 
-        elsif value == value
-          smallest_value = smallest_value[0]
          end 
        end
-       smallest_value
+       smallest_key 
      end 
 
   #if name_hash == name_hash
