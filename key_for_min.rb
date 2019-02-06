1df44ab7e6f2672b477678b3154b854627632ada
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-06 00:49:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
      smallest_value = nil #this will contain our lowest value
      smallest_key = nil  #this will contain the key for our lowest value
      name_hash.each do |name, value| 
-       if value < smallest_value #if the value in our hash is lower than our smallest_value, reassign smallest value to value
+       if smallest_value == nil || value < smallest_value #if the value in our hash is lower than our smallest_value, reassign smallest value to value
          smallest_value = value #reassigning smallest_value to lowest value 
          smallest_key = name 
          end 
